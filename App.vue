663097140d473d233cdc829e9f469e8b48d4b75c
GovindKota <102669668+GovindKota@users.noreply.github.com>
2022-03-31 19:29:38 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  <!--  <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Demo/>
+    <Tableview/>
+  </div>
+</template>
+
+<script>
+  
+//import HelloWorld from './components/HelloWorld.vue'
+import Demo from './components/Demo.vue'
+import Tableview from './components/Tableview.vue'
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+    Tableview,
+    Demo
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
