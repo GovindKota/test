1f8cd45c2933fa0066569b24781750e0baafb7ec
GovindKota <102669668+GovindKota@users.noreply.github.com>
2022-04-01 19:09:24 +0530

    Add files via upload

---
+++ function.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Function</h1>
+        <button @click="fun()">click</button>
+    </div>
+</template>
+<script>
+
+export default {
+    name:"FunctioN",
+    data() {
+        return {
+
+a:[
+{name:"Govind",Phone:"2222222222"},
+{name:"Manikanta",Phone:"5555555555"},
+{name:"srinu",Phone:"33333333"},
+]
+         }     
+        
+    },
+    methods: {
+        fun(){
+            console.log(this.a);
+        }
+    }
+}
+</script>
