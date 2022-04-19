e8da71ef4f214e6dabf27a0ae197820efe0d1264
GovindKota <govinda.kota@500apps.com>
2022-04-19 19:17:04 +0530

    19-04-2022

---
+++ testing1/src/components/AxioS.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+       <b-form-input v-model="value" placeholder="enter country name" required></b-form-input>
+    <button @click="getData()">click</button>
+    <b-table striped hover :items="posts" :fields="fields"> </b-table>
+    </div>
+</template>
+<script>
+const axios = require('axios').default;
+export default {
+    data() {
+        return{
+            posts:"",
+            fields:["name", "domains", "state-province", "web-pages"],
+        }
+        
+    },
+    methods:{
+     async getData() {
+      //try {
+        let response = await axios.get("http://universities.hipolabs.com/search?country=" + this.value);
+        //this.posts = await response.json();
+      //} 
+      //catch (error) {
+        console.log(response);
+      //}
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
---
+++ testing1/src/components/assignment-7/QueN7.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button @click="func()">Click</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"QueN7",
+    data() {
+        return{
+            nums : [1, 2, 2, 3, 1, 2, 4, 5, 4, 2, 6],
+        }
+    },
+    methods:{
+        func() {
+            document.write([...new Set(this.nums)]);
+        }
+    }
+}
+</script>
