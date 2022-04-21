4bfbff9ab508844678a5fd820a535d8584ba99d5
GovindKota <govinda.kota@500apps.com>
2022-04-21 19:26:55 +0530

    ES6 concepts

--- testing1/src/components/assignment-8/QueN1.vue
+++ testing1/src/components/assignment-8/QueN1.vue
@@ -1,30 +1,47 @@
 <template>
   <div>
-    <form name="myform" method="post" onsubmit="func()"></form>
-    <input type="text" name="word" placeholder="Enter Text" />
-    <button @click="func()">Search</button>
+    <!-- <form name="myform" method="post" onsubmit="myfun()">-->
+       <input type="text"  v-model="res" placeholder="Enter Text" />
+      <b-button @click="myfun">Map</b-button>
+      <b-button @click="fun">ForEach</b-button>
+      <b-button @click="func()">Filter</b-button>
+      <b-button @click="f1()">Find</b-button>
+    <!-- </form> -->
   </div>
 </template>
 <script>
 export default {
-  name: "QueN1",
+  name: "QuestioN1",
   data() {
     return {
-      search: "",
-      array: ["agile", "crm", "mantra", "technologies", "500apps", "infinity"],
-    };
+      array: ["agile", "crm", "mantra","technologies","500apps", "infinity"],
+      res:"",
+    }
   },
   methods: {
-    func() {
-      if (this.search) {
-        return this.array.filter((item) => {
-          return this.search.toLowerCase().split(" ").every((v) => item.title.toLowerCase().includes(v));
-        });
-      } 
-      else {
-        return this.array;
-      }
+    myfun() {
+      var res = this.array.map(r=>{
+        return r.name
+      })
+      console.log(res);
     },
-  },
-};
+fun(){
+  this.array.forEach((row) => {
+    console.log(row);
+  })
+},
+func(){
+   var value = this.array.filter((row)=>{
+     return row.includes(this.res);
+   })
+   console.log(value);
+}
+    },
+    f1(){
+      var value=this.array.find((row)=>{
+          return row.includes("ag");
+      })
+      console.log(value);
+    }
+}
 </script>
