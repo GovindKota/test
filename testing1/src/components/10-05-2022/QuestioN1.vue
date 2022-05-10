85712f87e1b42d4607a79900d9feffc97653d8af
GovindKota <govinda.kota@500apps.com>
2022-05-10 17:13:38 +0530

    10-05-2022

---
+++ testing1/src/components/10-05-2022/QuestioN1.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+   <b-table :items="items" :fields="fields" align="center"></b-table>
+    </div>
+</template>
+<script>
+export default {
+    name:"BtablE",
+    data() {
+        return{
+            fields:["productName","productPrice","productCategory"],
+          items:[
+              productName = "mantra",
+              productPrice = 10000000,
+              productCategory = "technologies"
+          ]
+        }
+    },
+}
+</script>
+<style scoped>
+b-table {
+  font-family: fantasy;
+  text-align: center;
+  color: white;
+  margin-top: 70px;
+  border: 2px;
+}
+</style>
