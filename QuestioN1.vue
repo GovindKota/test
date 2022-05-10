b3e66e590405ea12dfa1be79ec8f28f6e250188b
Govind-500apps <govinda.kota@500apps.com>
2022-05-10 17:33:56 +0530

    Add files via upload

---
+++ QuestioN1.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-form id="form">
+      Enter product name:<b-form-input id="name" class="name" placeholder="Enter product name"></b-form-input>
+      Enter product price:<b-form-input id="price"  class="prices" placeholder="Enter product price:" ></b-form-input>
+      Enter product category:<b-form-input id="category" class="categories" placeholder="Enter product category"></b-form-input>
+      <b-button @click="Add()">Add</b-button><br /><br />
+    </b-form>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  name: "QuestioN1",
+  data() {
+    return {
+      items: [
+        {
+          productname: "mantra",
+          productprice: 10000000,
+          productcategory: "technologies",
+        },
+      ],
+
+      fields: [],
+    };
+  },
+  methods: {
+    Add() {
+      this.items.push({
+        productname: document.getElementById("name").value,
+        productprice: document.getElementById("price").value,
+        productcategory: document.getElementById("category").value,
+      });
+    },
+  },
+};
+</script>
